70f245e42caed73ba453cef996461c01ff459200
Minh Ha-Duong <minh.haduong@vietse.vn>
2023-05-16 13:10:07 +0200

    Move indicators definitions to data.py.

--- data.py
+++ data.py
@@ -14,8 +14,42 @@ Created on Tue May  9 19:43:16 2023
 """
 
 import numpy as np
+from ar6_trajectories import df as df_ar6_trajectories
+from simulations import array_simulations
 
-simulations = np.load("simulations.npy")
+
+# %% This is a list not a set, order matters when we store in an numpy array
+
+indicators = [
+    "Emissions|CO2",  # total net-CO2 emissions from all sources, Mt CO2/yr
+    "GDP|MER",        # GDP at market exchange rate, billion US$2010/yr
+    "Population",
+    "Primary Energy"
+    ]
+
+"""
+Note: VN Masterplan 2021-2030 objectives pertain to:
+    Average GDP growth
+    GDP per capita
+    Urbanization rate
+    Propostion of GDP in services / industry+construction / agriculture+forestry+fisheries
+    Propostion of GDP of Digital economy
+    Population size
+    HDI
+    Life Expectancy and Healthy life Expectancy
+    Residential floor per person in urban area
+    Hospital beds and doctors per 10.000 people
+    Forest cover %, land reserves area, marine and coastal protected areas %
+    Rate of wastewater treatment
+    MSW collected %
+    GHG intensity of GDP
+"""
+# %%
+
+simulations = array_simulations(df_ar6_trajectories, indicators)
+
+# %% 
+
 observations = np.load("observations.npy")
 
 labels = np.concatenate([
--- simulations.py
+++ simulations.py
@@ -8,90 +8,48 @@ Created on Thu Apr 28 2023
 import pandas as pd
 import numpy as np
 
-from ar6_trajectories import df, top_variables
-
-# %% This is a list not a set, order matters when we store in an numpy array
-
-indicators = [
-    "Emissions|CO2",  # total net-CO2 emissions from all sources, Mt CO2/yr
-    "GDP|MER",        # GDP at market exchange rate, billion US$2010/yr
-    "Population",
-    "Primary Energy"
-    ]
-
-"""
-Note: VN Masterplan 2021-2030 objectives pertain to:
-    Average GDP growth
-    GDP per capita
-    Urbanization rate
-    Propostion of GDP in services / industry+construction / agriculture+forestry+fisheries
-    Propostion of GDP of Digital economy
-    Population size
-    HDI
-    Life Expectancy and Healthy life Expectancy
-    Residential floor per person in urban area
-    Hospital beds and doctors per 10.000 people
-    Forest cover %, land reserves area, marine and coastal protected areas %
-    Rate of wastewater treatment
-    MSW collected %
-    GHG intensity of GDP
-"""
-
-# Check we picked popular indicators
-print('Number of scenario x country simulations available, by variable')
-print(top_variables(df, -1)[indicators])
-print()
 
 # %% Keep only the rows describing the indicators
 
 
-def hasall(df, indicators):
-    """Return a mask so that the subdataframe df[mask] is 'full' wrt indicators.
+def _select(df, indicators):
+    """Return a masked dataframe with rows to include the variables in the indicators list.
 
-    'Full' meaning for any triple (model, scenario, region),
-    the set of variables V so that rows (model, scenario, region, V) are present
-    is exactly the set 'indicators'.
-    We return the largest such subdataframe.
+    For any triple (model, scenario, region),
+    the result contains the row (model, scenario, region, v) for any v in indicators
+    the result does not contains any other row
+    the result is the largest such subdataframe.
     """
+    assert not df.empty, "The dataframe to select from must not be empty."
+    assert indicators, "Indicators must be a non-empty list."
+    
     mask = pd.Series(
         df.index.get_level_values('Variable').isin(indicators),
         index=df.index)
     group_counts = mask.groupby(level=[0, 1, 2]).transform('sum')
     mask[group_counts != len(indicators)] = False
-    return mask
 
-df = df[hasall(df, indicators)]
+    result = df[mask]
 
+    assert list(result.index.get_level_values('Variable').unique()) == indicators, "Incorrect Variables selection."
+    assert len(result['Unit'].unique()) == len(indicators), "More units than variables."
+    assert not result.empty, "The result is empty. Too many indicators?"
 
-# %% Sanity check
-
-units = df['Unit'].unique() 
-print('Units:')
-print(units)
-print()
-assert len(units) == len(indicators), "More units than variables."
-del units
+    return result
 
 
 # %% Make a numpy array of 25-years trajectories
 
-width = 6   # At five years step, twenty five years including extremities
+def array_simulations(df, indicators):
+    subdf = _select(df, indicators)
 
-simulations = np.array([
-    a[:, i:i + width]
-    for _, trajectory in df.groupby(level=[0, 1, 2])
-    for a in [trajectory.iloc[:, 1:].values]
-    for i in range(df.shape[1] - width)
-    if not (np.isnan(a) | (a == 0)).any()
-])
-
-del width
-
-
-# %%
-
-try:
-    np.save('simulations.npy', simulations)
-    print('Array simulations saved successfully!')
-except Exception as e:
-    print('An error occurred while saving the array simulations:', e)
+    width = 6   # At five years step, twenty five years including extremities
+    num_trajectories = df.shape[1] - width
+    result = np.array([
+        a[:, i:i + width]
+        for _, trajectory in subdf.groupby(level=[0, 1, 2])
+        for a in [trajectory.iloc[:, 1:].values]
+        for i in range(num_trajectories)
+        if not (np.isnan(a) | (a == 0)).any()
+        ])
+    return result
