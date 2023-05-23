39f1c16553c57f46a1a51e2e0e3ef84b69144cb7
Minh Ha-Duong <minh.haduong@vietse.vn>
2023-05-23 22:51:11 +0200

    Refactor sequences2array into data.py.

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from simulations import get_simulations
-from observations import get_observations
+from owid_sequences import df_sequences as df_observations
+from ar6_sequences import df_sequences as df_simulations
 
 
 # Source ourworldindata.org
@@ -32,6 +32,35 @@ def _as_change(arrays):
     return (arrays / rotated)[:, :, 1:]
 
 
+def sequence2array(df, indicators, units, level_names, group_keys):
+    """Return data array after applying the select function and adjusting units."""
+
+    assert indicators, "Indicators must be a non-empty list."
+    assert not df.empty, "DataFrame to select from must not be empty."
+    assert not df.isna().any().any(), "DataFrame must not contains NaN values"
+    assert not (df == 0).any().any(), "DataFrame must not contains zero values"
+
+    mask = pd.Series(
+        df.index.get_level_values(level_names).isin(indicators), index=df.index
+    )
+    group_counts = mask.groupby(level=group_keys).transform("sum")
+    mask[group_counts != len(indicators)] = False
+
+    subdf = df[mask].sort_index()
+
+    assert set(subdf.index.get_level_values(level_names).unique()) == set(
+        indicators
+    ), "Incorrect Variables selection."
+    assert (
+        not subdf.empty
+    ), "The result is empty. Match the list of indicators to observations data."
+
+    subdf = subdf.div(units, level=level_names, axis=0)
+
+    result = np.array([a for _, a in subdf.groupby(level=group_keys)])
+    return result
+
+
 def get_data(var=None, as_change=None):
     """
     Combine simulations and observations.
@@ -39,13 +68,21 @@ def get_data(var=None, as_change=None):
     Label simulations as 0 and observations as 1.
     Return a tuple of two numpy arrays of the same length: data, labels.
     If called with no argument, will use all indicators.
-    Assumes that the two arguments are aligned lists of variables.
     """
     if var is None:
         var = all_vars
 
-    simulations = get_simulations(var, units=world_1990)
-    observations = get_observations(var, units=world_1990)
+    observations = sequence2array(
+        df_observations, var, world_1990, "variable", ["country", "year"]
+    )
+
+    simulations = sequence2array(
+        df_simulations,
+        var,
+        world_1990,
+        "Variable",
+        ["Model", "Scenario", "Region", "Year"],
+    )
 
     print(
         f"{var}    {len(simulations)} simulations, {len(observations)} observation sequences"
--- simulations.py
+++ simulations.py
@@ -5,49 +5,58 @@ Created on Thu Apr 28 2023
 @author: haduong@centre-cired.fr
 """
 
-import pandas as pd
-import numpy as np
+from sequence2array import sequence2array
 from ar6_sequences import df_sequences
 
 
-def _select(df, indicators):
-    """Return a masked dataframe with rows to include the variables in the indicators list.
-
-    For any pair (country, year),
-    the result contains the rows (country, year, v) for any v in indicators
-    the result does not contains any other row
-    the result is the largest such subdataframe of df
-    """
-    assert indicators, "Indicators must be a non-empty list."
-    assert not df.empty, "DataFrame to select from must not be empty."
-    assert not df.isna().any().any(), "DataFrame must not contains NaN values"
-    assert not (df == 0).any().any(), "DataFrame must not contains zero values"
-
-    mask = pd.Series(
-        df.index.get_level_values("Variable").isin(indicators), index=df.index
-    )
-    group_counts = mask.groupby(
-        level=["Model", "Scenario", "Region", "Year"]
-    ).transform("sum")
-    mask[group_counts != len(indicators)] = False
-
-    result = df[mask].sort_index()
-
-    assert set(result.index.get_level_values("Variable").unique()) == set(
-        indicators
-    ), "Incorrect Variables selection."
-    assert (
-        not result.empty
-    ), "The result is empty. Match the list of indicators to observations data."
-
-    return result
-
-
 def get_simulations(indicators, units=1):
-    subdf = _select(df_sequences, indicators)
-    subdf = subdf.div(units, level="Variable", axis=0)
-
-    result = np.array(
-        [a for _, a in subdf.groupby(level=["Model", "Scenario", "Region", "Year"])]
+    return sequence2array(
+        df_sequences,
+        indicators,
+        units,
+        "Variable",
+        ["Model", "Scenario", "Region", "Year"],
     )
-    return result
+
+
+# def _select(df, indicators):
+#     """Return a masked dataframe with rows to include the variables in the indicators list.
+
+#     For any pair (country, year),
+#     the result contains the rows (country, year, v) for any v in indicators
+#     the result does not contains any other row
+#     the result is the largest such subdataframe of df
+#     """
+#     assert indicators, "Indicators must be a non-empty list."
+#     assert not df.empty, "DataFrame to select from must not be empty."
+#     assert not df.isna().any().any(), "DataFrame must not contains NaN values"
+#     assert not (df == 0).any().any(), "DataFrame must not contains zero values"
+
+#     mask = pd.Series(
+#         df.index.get_level_values("Variable").isin(indicators), index=df.index
+#     )
+#     group_counts = mask.groupby(
+#         level=["Model", "Scenario", "Region", "Year"]
+#     ).transform("sum")
+#     mask[group_counts != len(indicators)] = False
+
+#     result = df[mask].sort_index()
+
+#     assert set(result.index.get_level_values("Variable").unique()) == set(
+#         indicators
+#     ), "Incorrect Variables selection."
+#     assert (
+#         not result.empty
+#     ), "The result is empty. Match the list of indicators to observations data."
+
+#     return result
+
+
+# def get_simulations(indicators, units=1):
+#     subdf = _select(df_sequences, indicators)
+#     subdf = subdf.div(units, level="Variable", axis=0)
+
+#     result = np.array(
+#         [a for _, a in subdf.groupby(level=["Model", "Scenario", "Region", "Year"])]
+#     )
+#     return result
