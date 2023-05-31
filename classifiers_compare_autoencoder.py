d352e8fcbd16e12b9d8a5b105ec4c5cd6bc25a83
Minh Ha-Duong <minh.haduong@vietse.vn>
2023-05-31 18:22:11 +0200

    Add dropout, early stopping, use normalized from data.py.

--- classifiers_compare_autoencoder.py
+++ classifiers_compare_autoencoder.py
@@ -9,11 +9,10 @@ import logging
 import pandas as pd
 
 from data import get_sets, all_vars
-from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import SMOTE
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
 
 from sklearn.decomposition import PCA
 
@@ -43,13 +42,17 @@ def perform_pca(x_raw_train, x_raw_test, n_components=NEWDIM):
 
 # Function for autoencoder dimensionality reduction
 def perform_autoencode(
-    x_raw_train, x_raw_test, latent_dim=NEWDIM, epochs=50, batch_size=32, n=1
+    x_raw_train, x_raw_test, latent_dim=NEWDIM, epochs=100, batch_size=32, n=1
 ):
-    logging.info(f"Training an autoencoder to a {latent_dim} dimensions latent space.")
+    logging.info(
+        f"Training autoencoder: dim {x_raw_train.shape[1]} -> dim {latent_dim}."
+    )
     autoencoder = Sequential()
     autoencoder.add(Dense(20, activation="relu", input_shape=(x_raw_train.shape[1],)))
+    autoencoder.add(Dropout(0.05))
     if n == 2:
         autoencoder.add(Dense(20, activation="relu"))
+        autoencoder.add(Dropout(0.05))
     autoencoder.add(Dense(latent_dim, activation="linear"))
     autoencoder.add(Dense(20, activation="relu"))
     if n == 2:
@@ -57,10 +60,17 @@ def perform_autoencode(
     autoencoder.add(Dense(x_raw_train.shape[1], activation="linear"))
 
     autoencoder.compile(optimizer="adam", loss="mse")
-    autoencoder.fit(x_raw_train, x_raw_train, epochs=epochs, batch_size=batch_size)
+    autoencoder.fit(
+        x_raw_train,
+        x_raw_train,
+        epochs=epochs,
+        batch_size=batch_size,
+        callbacks=[EarlyStopping(monitor="val_loss", patience=3)],
+        validation_data=(x_raw_test, x_raw_test),
+    )
 
     # Obtain the latent space representation
-    encoder = Sequential(autoencoder.layers[:2])
+    encoder = Sequential(autoencoder.layers[: (1 + 2 * n)])
     x_latent_train = encoder.predict(x_raw_train)
     x_latent_test = encoder.predict(x_raw_test)
 
@@ -71,10 +81,12 @@ def perform_autoencode(
 
 results = pd.DataFrame(columns=["result", "duration"])
 
-x_raw_train, x_raw_test, y_train, y_test = get_sets(all_vars, as_change=True)
+x_base_train, x_base_test, y_train, y_test = get_sets(
+    all_vars, diff=True, normalize=True, rebalance=True
+)
 
 
-def models_dict(input_dim=x_raw_train.shape[1]):
+def models_dict(input_dim=x_base_train.shape[1]):
     return {
         "Dummy baseline": model_dummy,
         "Gradient boosting machine": model_xgb,
@@ -84,40 +96,34 @@ def models_dict(input_dim=x_raw_train.shape[1]):
     }
 
 
-scaler = StandardScaler()
-x_train_scaled = scaler.fit_transform(x_raw_train)
-x_test_scaled = scaler.transform(x_raw_test)
-smote = SMOTE(random_state=0)
-x_base_train, y_base_train = smote.fit_resample(x_train_scaled, y_train)
-
 results.loc["normalized"] = compare(
-    models_dict(), x_base_train, x_test_scaled, y_base_train, y_test
+    models_dict(), x_base_train, x_base_test, y_train, y_test
 )
 
-x_pca_train, x_pca_test = perform_pca(x_base_train, x_test_scaled)
+x_pca_train, x_pca_test = perform_pca(x_base_train, x_base_test)
 assert x_pca_train.shape[1] == NEWDIM
 results.loc["PCA"] = compare(
-    models_dict(x_pca_train.shape[1]), x_pca_train, x_pca_test, y_base_train, y_test
+    models_dict(NEWDIM), x_pca_train, x_pca_test, y_train, y_test
 )
 
 
-x_latent_train, x_latent_test = perform_autoencode(x_base_train, x_test_scaled)
+x_latent_train, x_latent_test = perform_autoencode(x_base_train, x_base_test)
 assert x_latent_train.shape[1] == NEWDIM
 results.loc["latent"] = compare(
-    models_dict(x_latent_train.shape[1]),
+    models_dict(NEWDIM),
     x_latent_train,
     x_latent_test,
-    y_base_train,
+    y_train,
     y_test,
 )
 
-x_latent_train, x_latent_test = perform_autoencode(x_base_train, x_test_scaled, n=2)
+x_latent2_train, x_latent2_test = perform_autoencode(x_base_train, x_base_test, n=2)
 assert x_latent_train.shape[1] == NEWDIM
 results.loc["latent2"] = compare(
-    models_dict(x_latent_train.shape[1]),
-    x_latent_train,
-    x_latent_test,
-    y_base_train,
+    models_dict(NEWDIM),
+    x_latent2_train,
+    x_latent2_test,
+    y_train,
     y_test,
 )
 
