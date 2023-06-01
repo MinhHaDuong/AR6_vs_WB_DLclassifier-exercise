27f7a3d3819096f0bf856c959c7fe1e7d51fc56e
Minh Ha-Duong <minh.haduong@vietse.vn>
2023-06-01 10:35:27 +0200

    Split run/print files.

--- classifiers_compare_kind.py
+++ classifiers_compare_kind.py
@@ -1,80 +1,79 @@
-""" Compare different machine learning algorithms to recognize simulations
+""" Compare different ML classifiers observations/simulations.
+
+Results are cached in .pkl for further processing.
+
 Created on Tue May 30 12:42:04 2023
 
 @author: haduong
 """
 
 import pandas as pd
+import pickle
+import logging
 
 from classifier_others import model_dummy, model_lr, model_rf, model_svm, model_xgb
 from classifier_mlp import model_mlp
-from classifiers_compare import compare, pretty_print
+from classifiers_compare import compare
 
 from data import get_sets
+from log_config import setup_logger
+
+setup_logger()
+logger = logging.getLogger(__name__)
+
+FILENAME="classifiers_compare_kind.pkl"
+
+def get_results(filename):
+    # Compare our default with the top four architectures from the model tuner
+    x_train, _, _, _ = get_sets(diff=True)
+    dim = x_train.shape[1]
+    models_dict = {
+        "Dummy baseline": model_dummy,
+        "Logistic regression": model_lr,
+        "Support vector machine": model_svm,
+        "Random forest": model_rf,
+        "Gradient boosting machine": model_xgb,
+        "Multilayer perceptron default": model_mlp(dim),
+        "Multilayer perceptron bis": model_mlp(dim),
+        "Multilayer perceptron ter": model_mlp(dim),
+        "Multilayer perceptron 128/0/48/0.1/16/0": model_mlp(dim, 128, 0, 48, 0.1, 16, 0),
+        "Multilayer perceptron 96/0/48/0.2/8/0.2": model_mlp(dim, 96, 0, 48, 0.2, 8, 0.2),
+        "Multilayer perceptron 64/0/32/0/32/0.1": model_mlp(dim, 64, 0, 32, 0, 32, 0.1),
+        "Multilayer perceptron 32/0.1/32/0.1/32/0.2": model_mlp(
+            dim, 32, 0.1, 32, 0.1, 32, 0.2
+        ),
+    }
+
+    results = pd.DataFrame(columns=["result", "duration"])
+
+    def run(case, normalize, rebalance):
+        x_train, x_test, y_train, y_test = get_sets(
+            diff=True, normalize=normalize, rebalance=rebalance
+        )
+        results.loc[case] = compare(
+            models_dict, x_train, x_test, y_train, y_test, parallelize=False
+        )
+        results.loc["parallel " + case] = compare(
+            models_dict, x_train, x_test, y_train, y_test, parallelize=True
+        )
+
+    run("raw", False, False)
+    run("normalized", True, False)
+    run("balanced", False, True)
+    run("base", True, True)
+
+    return results
 
 
-# %% Run the comparison.
-# Compare our default with the top four architectures from the model tuner
-
-x_train, _, _, _ = get_sets(diff=True)
-dim = x_train.shape[1]
-models_dict = {
-    "Dummy baseline": model_dummy,
-    "Logistic regression": model_lr,
-    "Support vector machine": model_svm,
-    "Random forest": model_rf,
-    "Gradient boosting machine": model_xgb,
-    "Multilayer perceptron default": model_mlp(dim),
-    "Multilayer perceptron bis": model_mlp(dim),
-    "Multilayer perceptron ter": model_mlp(dim),
-    "Multilayer perceptron 128/0/48/0.1/16/0": model_mlp(dim, 128, 0, 48, 0.1, 16, 0),
-    "Multilayer perceptron 96/0/48/0.2/8/0.2": model_mlp(dim, 96, 0, 48, 0.2, 8, 0.2),
-    "Multilayer perceptron 64/0/32/0/32/0.1": model_mlp(dim, 64, 0, 32, 0, 32, 0.1),
-    "Multilayer perceptron 32/0.1/32/0.1/32/0.2": model_mlp(
-        dim, 32, 0.1, 32, 0.1, 32, 0.2
-    ),
-}
-
-results = pd.DataFrame(columns=["result", "duration"])
-
-
-def run(case, normalize, rebalance):
-    x_train, x_test, y_train, y_test = get_sets(
-        diff=True, normalize=normalize, rebalance=rebalance
-    )
-    results.loc[case] = compare(
-        models_dict, x_train, x_test, y_train, y_test, parallelize=False
-    )
-    results.loc["parallel " + case] = compare(
-        models_dict, x_train, x_test, y_train, y_test, parallelize=True
-    )
-
-
-run("raw", False, False)
-run("normalized", True, False)
-run("balanced", False, True)
-run("base", True, True)
-
-
-# %% Print and save the profiled results
-
-pd.set_option("display.max_columns", None)
-pd.set_option("display.width", 10000)
-
-keys = [
-    "raw",
-    "parallel raw",
-    "normalized",
-    "parallel normalized",
-    "balanced",
-    "parallel balanced",
-    "base",
-    "parallel base",
-]
-
-print(pretty_print(results, keys, "to_string"))
-
-with open("classifiers_compare_kind.csv", "w", encoding="utf-8") as f:
-    print(pretty_print(results, keys, "to_csv", sep="\t"), file=f)
-
-results.to_pickle("classifiers_compare_kind.pkl")
+try:
+    results = pd.read_pickle(FILENAME)
+    logging.info(f"Success read  file {FILENAME} ")
+except (IOError, EOFError, pickle.UnpicklingError) as e_read:
+    logging.info(f"Unable to access {FILENAME} : {e_read}")
+    logging.info("Attempting to create it.")
+    try:
+        results = get_results()
+        results.to_pickle(FILENAME)
+        logging.info("Classifiers comparison saved successfully!")
+    except Exception as e:
+        logging.error(f"An error occurred while saving the classifiers comparison: {e}")
---
+++ classifiers_tables_kind.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 10:07:10 2023
+
+@author: haduong
+"""
+import pandas as pd
+
+from classifiers_compare import pretty_print
+from classifiers_compare_kind import results
+
+
+# %% Print and save the profiled results
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 10000)
+
+keys = [
+    "raw",
+    "parallel raw",
+    "normalized",
+    "parallel normalized",
+    "balanced",
+    "parallel balanced",
+    "base",
+    "parallel base",
+]
+
+print(pretty_print(results, keys, "to_string"))
+
+with open("classifiers_compare_kind.csv", "w", encoding="utf-8") as f:
+    print(pretty_print(results, keys, "to_csv", sep="\t"), file=f)
